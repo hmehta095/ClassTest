88b46e2831413fcfbc01d2fb494d22e6ab1793da
himanshumehta <himanshumehta@10.51.212.113>
2019-09-12 12:21:41 -0400

    Tc2 Half

--- BlazeDemoTest/BlazeDemoTest.java
+++ BlazeDemoTest/BlazeDemoTest.java
@@ -10,28 +10,46 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BlazeDemoTest {
 
+	WebDriver driver;
 	
 	@Before
 	public void setUp() throws Exception {
+//		System.setProperty("webdriver.chrome.driver","/Users/himanshumehta/Desktop/chromedriver");
+//		WebDriver driver = new ChromeDriver();
+//		driver.get("http://blazedemo.com/");
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		Thread.sleep(1000);
+		driver.close();
 	}
 
 	@Test
-	public void test() {
+	public void test() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver","/Users/himanshumehta/Desktop/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://blazedemo.com/");
-//		WebElement popScreen = driver.findElement(By.cssSelector("a.exit"));
-//		popScreen.click();
-		WebElement SubscribeText = driver.findElement(By.name("fromPort"));
-		String SubscribeTextData = SubscribeText.getText();
 
-		System.out.println(SubscribeTextData);
-		assertEquals("Paris\n  Philadelphia\n  Boston\n  Portland\n  San Diego\n  Mexico City\n  São Paolo", SubscribeTextData);
+		WebElement DepartCity = driver.findElement(By.name("fromPort"));
+		String DepartCityData = DepartCity.getText();
 
+		System.out.println(DepartCityData);
+//		assertEquals("7", SubscribeTextData);
+		Thread.sleep(1000);
+		driver.close();
+	}
+	
+	@Test
+	public void test2() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","/Users/himanshumehta/Desktop/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://blazedemo.com/");
+		WebElement Flight = driver.findElement(By.name("VA12"));
+		String FlightData = Flight.getText();
+		System.out.println(FlightData);
+		Thread.sleep(1000);
+		driver.close();
 	}
 
 }
